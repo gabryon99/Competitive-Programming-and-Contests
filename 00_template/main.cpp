0afc19356a93f9ff9f87e3dca57434df82d20c69
gabryon99 <leleattolo99@gmail.com>
2021-09-15 16:54:55 +0200

    add template code for future exercises

---
+++ 00_template/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @readme: Template file to be used for the exercises.
+ * @brief Text of the problem.
+ */
+
+#include <cstdio>
+
+auto main(int argc, char** argv) -> int {
+    std::fprintf(stdout, "Hello World!\n");
+    return 0;
+}
